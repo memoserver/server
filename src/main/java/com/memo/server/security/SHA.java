9c030ac76295131c30f2c0074c2244db63f4a041
yunqifanerna <18862020369@163.com>
2019-03-18 00:05:26 +0800

    SHA里面改了部分名字

--- src/main/java/com/memo/server/security/SHA.java
+++ src/main/java/com/memo/server/security/SHA.java
@@ -1,33 +1,34 @@
 package com.memo.server.security;
-
 import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class SHA {
     public static final String KEY_SHA = "SHA";
 
-    public static  String  getResult(String text)
-    {
-        //cipherthext
-    //}
-        BigInteger sha =null;
-        System.out.println("=======加密前的数据:"+inputStr);
-        byte[] inputData = inputStr.getBytes();
+    public static String getResult(String text) {
+        //ciphertext
+        //}
+        BigInteger sha = null;
+        System.out.println("=======加密前的数据:" + text);
+        byte[] inputData = text.getBytes();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(KEY_SHA);
             messageDigest.update(inputData);
             sha = new BigInteger(messageDigest.digest());
             System.out.println("SHA加密后:" + sha.toString(32));
-        } catch (Exception e) {e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return sha.toString(32);
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         try {
-            String inputStr = "简单加密";
-            getResult(inputStr);
+            String text = "1234";
+            getResult(text);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+}
